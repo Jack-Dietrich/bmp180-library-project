fc18d7bf9c6c76d2bbd53c127ea0652b7c9da111
Jack Dietrich <jed123@rogers.com>
2025-06-21 17:13:55 -0400

    Changed working of write, figured out that we only send 7 bit addr.

--- src/main.cpp
+++ src/main.cpp
@@ -25,100 +25,134 @@ void setup() {
   //from page 15 of the manual
   //Step 1, read calibration data from eeprom, 16 bits, MSB first
 
+  /*
+  77 address is for writing
+  77 addr is for reading
+  -There may be a problem with 7 or 8 bit addressing with arduino wire library
+  */
+
   Serial.begin(9600);
   Serial.println("Entering setup");
-  Wire.begin();
-  Wire.beginTransmission(0xEF);//this is the module address //after this we should have an ack from the bmp180
-  //we are beginning with EF as this means we want to read the eeprom first
-  Serial.println("here1");
+  Wire.begin(); //this should send the start condition
+  
+  
+  Wire.beginTransmission(0x77);//this is the module address //after this we should have an ack from the bmp180
 
-  Wire.requestFrom(0xAA,1); //requesting one byte from the upper address first
+  Wire.write(0xAA);//write the reg we want to read from //this just queues them
 
-  Serial.println("here2");
-  AC1 = Wire.read();//reads in the requested data
-  AC1 = AC1 << 8; //shift left by 8 bits since we read in MSB FIRST
-  Serial.print("val befr concat:"); Serial.print(AC1);
+  //should get an asks msg here
 
-  Wire.requestFrom(0xAB,1); //requesting one byte from lower address
-  AC1 = AC1 | Wire.read();//reads in the requested data
-  Serial.print("val aftr concat:"); Serial.print(AC1); //for debugging
+  Wire.endTransmission(); //this actually writes them
 
-  Wire.requestFrom(0xAC,1);
-  AC2 = AC2 << 8; //shift left by 8 bits since we read in MSB FIRST
-  AC2 = Wire.read();
+  Wire.beginTransmission(0x77); //now reading
 
-  Wire.requestFrom(0xAD,1);
-  AC2 = AC2 | Wire.read();
-
-  //
-  Wire.requestFrom(0xAE,1);
-  AC3 = Wire.read();
-  AC3 = AC3 << 8; //shift left by 8 bits since we read in MSB FIRST
-
-
-  Wire.requestFrom(0xAF,1);
-  AC3 = AC3 | Wire.read();
-
-  Wire.requestFrom(0xB0,1);
-  AC4 = Wire.read();
-  AC4 = AC4 << 8; //shift left by 8 bits since we read in MSB FIRST
-
-
-  Wire.requestFrom(0xB1,1);
-  AC4 = AC4 | Wire.read();
-
-  Wire.requestFrom(0xB2,1);
-  AC5 = Wire.read();
-  AC5 = AC5 << 8; //shift left by 8 bits since we read in MSB FIRST
-
-
-  Wire.requestFrom(0xB3,1);
-  AC5 = AC5 | Wire.read();
-
-  Wire.requestFrom(0xB4,1);
-  AC6 = Wire.read();
-  AC6 = AC6 << 8; //shift left by 8 bits since we read in MSB FIRST
-
-  Wire.requestFrom(0xB5,1);
-  AC6 = AC6 | Wire.read();
-
-  Wire.requestFrom(0xB6,1);
-  BB1 = Wire.read();
-  BB1 = BB1 << 8; //shift left by 8 bits since we read in MSB FIRST
-
-  Wire.requestFrom(0xB7,1);
-  BB1 = BB1 | Wire.read();
-
-  Wire.requestFrom(0xB8,1);
-  B2 = Wire.read();
-  B2 = B2 << 8; //shift left by 8 bits since we read in MSB FIRST
-
-  Wire.requestFrom(0xB9,1);
-  B2 = B2 | Wire.read();
-
-  Wire.requestFrom(0xBA,1); //lower case a in the docs may be a typo here?
-  MB = Wire.read();
-  MB = MB << 8; //shift left by 8 bits since we read in MSB FIRST
-
-  Wire.requestFrom(0xBB,1);
-  MB = MB | Wire.read();
-
-
-  Wire.requestFrom(0xBC,1);
-  MC = Wire.read();
-  MC = MC << 8; //shift left by 8 bits since we read in MSB FIRST
-
-  Wire.requestFrom(0xBD,1);
-  MC = MC | Wire.read();
-
-  Wire.requestFrom(0xBE,1);
-  MD = Wire.read();
-  MD = MD << 8; //shift left by 8 bits since we read in MSB FIRST
-
-  Wire.requestFrom(0xBF,1);
-  MD = MD | Wire.read();
+  AC1 = Wire.read(); //read msb,
+  AC1 = AC1 << 8;
+  AC1 = AC1 | Wire.read(); //read lsb
 
   Wire.endTransmission();
+
+  // Read AC2
+  Wire.beginTransmission(0x77);
+  Wire.write(0xAC);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x77);
+  AC2 = Wire.read();
+  AC2 = AC2 << 8;
+  AC2 = AC2 | Wire.read();
+  Wire.endTransmission();
+
+  // Read AC3
+  Wire.beginTransmission(0x77);
+  Wire.write(0xAE);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x77);
+  AC3 = Wire.read();
+  AC3 = AC3 << 8;
+  AC3 = AC3 | Wire.read();
+  Wire.endTransmission();
+
+  // Read AC4
+  Wire.beginTransmission(0x77);
+  Wire.write(0xB0);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x77);
+  AC4 = Wire.read();
+  AC4 = AC4 << 8;
+  AC4 = AC4 | Wire.read();
+  Wire.endTransmission();
+
+  // Read AC5
+  Wire.beginTransmission(0x77);
+  Wire.write(0xB2);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x77);
+  AC5 = Wire.read();
+  AC5 = AC5 << 8;
+  AC5 = AC5 | Wire.read();
+  Wire.endTransmission();
+
+  // Read AC6
+  Wire.beginTransmission(0x77);
+  Wire.write(0xB4);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x77);
+  AC6 = Wire.read();
+  AC6 = AC6 << 8;
+  AC6 = AC6 | Wire.read();
+  Wire.endTransmission();
+
+  // Read BB1
+  Wire.beginTransmission(0x77);
+  Wire.write(0xB6);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x77);
+  BB1 = Wire.read();
+  BB1 = BB1 << 8;
+  BB1 = BB1 | Wire.read();
+  Wire.endTransmission();
+
+  // Read B2
+  Wire.beginTransmission(0x77);
+  Wire.write(0xB8);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x77);
+  B2 = Wire.read();
+  B2 = B2 << 8;
+  B2 = B2 | Wire.read();
+  Wire.endTransmission();
+
+  // Read MB
+  Wire.beginTransmission(0x77);
+  Wire.write(0xBA);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x77);
+  MB = Wire.read();
+  MB = MB << 8;
+  MB = MB | Wire.read();
+  Wire.endTransmission();
+
+  // Read MC
+  Wire.beginTransmission(0x77);
+  Wire.write(0xBC);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x77);
+  MC = Wire.read();
+  MC = MC << 8;
+  MC = MC | Wire.read();
+  Wire.endTransmission();
+
+  // Read MD
+  Wire.beginTransmission(0x77);
+  Wire.write(0xBE);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x77);
+  MD = Wire.read();
+  MD = MD << 8;
+  MD = MD | Wire.read();
+  Wire.endTransmission();
+
+
 
 
 }
@@ -126,10 +160,12 @@ void setup() {
 void loop() {
 
   //reading temp repeatedly
-
+  
   //we now have initalized everything, we move to the 2nd box on page 15 read uncompensated temperature value
 
-  Wire.beginTransmission(0xEE); //start transmission in write mode
+  Serial.println("In the loop");
+
+  Wire.beginTransmission(0x77); //start transmission in write mode
 
   Wire.write(0xF4);//SEND REGISTER ADDRESS
   Wire.write(0x2E);//write 2E into the address
@@ -141,7 +177,7 @@ void loop() {
 
   Wire.endTransmission(); //end transmission as we now want to read
 
-  Wire.beginTransmission(0xEF);
+  Wire.beginTransmission(0x77);
 
   Wire.requestFrom(0xF6,1);
   MSB = Wire.read();
