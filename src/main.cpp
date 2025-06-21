675c11c66a62703ff1beb5c1635b5a4909f449cb
Jack Dietrich <jed123@rogers.com>
2025-06-21 18:58:23 -0400

    Not yet functional, basic skeleton compiles and executes

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,25 @@ long UT = 0;
 long T = 0; //this is the final temp
 /*End of variable definitions*/
 
+//@param reg should be supplied in hex
+//@param var is a pointer to the variable that is to be read into
+void readReg(short reg, void* var){ //unsure if I can pass this as a void pointer, I need both short and unsigned short values
+  Wire.beginTransmission(0x77);//this is the module address only the first 7 bits
+
+  Wire.write(reg);//we want to read from this register
+
+  Wire.endTransmission(false); //end transmission, but we don't want to send stop, just restart as per datasheet
+
+  Wire.requestFrom(0x77,2,true); //we now request 2 bytes, from the bmp180, this should read from 0xAA and 0X AB respectfully (hopefully) // sends the stop (p) signal once we have read the register
+
+  AC1 = Wire.read();// read in MSB
+  AC1 = AC1 << 8; //shift over 8 bits so we can read in LSB
+  AC1 = AC1 | Wire.read();
+
+  //may need to end transmission each time?
+}
+
+
 void setup() {
   //from page 15 of the manual
   //Step 1, read calibration data from eeprom, 16 bits, MSB first
@@ -35,122 +54,25 @@ void setup() {
   Serial.println("Entering setup");
   Wire.begin(); //this should send the start condition
   
-  
-  Wire.beginTransmission(0x77);//this is the module address //after this we should have an ack from the bmp180
+  //READ a registers
+  readReg(0xAA,&AC1);
+  readReg(0xAC,&AC2);
+  readReg(0xAE,&AC3);
+  readReg(0xB0,&AC4);
+  readReg(0xB2,&AC5);
+  readReg(0xB4,&AC6);
 
-  Wire.write(0xAA);//write the reg we want to read from //this just queues them
+  readReg(0XB6,&BB1); //BB1 AS B1 IS RESERVED FOR OTHER FUNCTIONALITY
+  readReg(0xB8,&B2);
+  readReg(0xBA,&MB);
+  readReg(0xBC,&MC);
+  readReg(0xBE,&MD);
 
-  //should get an asks msg here
 
-  Wire.endTransmission(); //this actually writes them
 
-  Wire.beginTransmission(0x77); //now reading
 
-  AC1 = Wire.read(); //read msb,
-  AC1 = AC1 << 8;
-  AC1 = AC1 | Wire.read(); //read lsb
 
-  Wire.endTransmission();
 
-  // Read AC2
-  Wire.beginTransmission(0x77);
-  Wire.write(0xAC);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x77);
-  AC2 = Wire.read();
-  AC2 = AC2 << 8;
-  AC2 = AC2 | Wire.read();
-  Wire.endTransmission();
-
-  // Read AC3
-  Wire.beginTransmission(0x77);
-  Wire.write(0xAE);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x77);
-  AC3 = Wire.read();
-  AC3 = AC3 << 8;
-  AC3 = AC3 | Wire.read();
-  Wire.endTransmission();
-
-  // Read AC4
-  Wire.beginTransmission(0x77);
-  Wire.write(0xB0);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x77);
-  AC4 = Wire.read();
-  AC4 = AC4 << 8;
-  AC4 = AC4 | Wire.read();
-  Wire.endTransmission();
-
-  // Read AC5
-  Wire.beginTransmission(0x77);
-  Wire.write(0xB2);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x77);
-  AC5 = Wire.read();
-  AC5 = AC5 << 8;
-  AC5 = AC5 | Wire.read();
-  Wire.endTransmission();
-
-  // Read AC6
-  Wire.beginTransmission(0x77);
-  Wire.write(0xB4);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x77);
-  AC6 = Wire.read();
-  AC6 = AC6 << 8;
-  AC6 = AC6 | Wire.read();
-  Wire.endTransmission();
-
-  // Read BB1
-  Wire.beginTransmission(0x77);
-  Wire.write(0xB6);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x77);
-  BB1 = Wire.read();
-  BB1 = BB1 << 8;
-  BB1 = BB1 | Wire.read();
-  Wire.endTransmission();
-
-  // Read B2
-  Wire.beginTransmission(0x77);
-  Wire.write(0xB8);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x77);
-  B2 = Wire.read();
-  B2 = B2 << 8;
-  B2 = B2 | Wire.read();
-  Wire.endTransmission();
-
-  // Read MB
-  Wire.beginTransmission(0x77);
-  Wire.write(0xBA);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x77);
-  MB = Wire.read();
-  MB = MB << 8;
-  MB = MB | Wire.read();
-  Wire.endTransmission();
-
-  // Read MC
-  Wire.beginTransmission(0x77);
-  Wire.write(0xBC);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x77);
-  MC = Wire.read();
-  MC = MC << 8;
-  MC = MC | Wire.read();
-  Wire.endTransmission();
-
-  // Read MD
-  Wire.beginTransmission(0x77);
-  Wire.write(0xBE);
-  Wire.endTransmission();
-  Wire.beginTransmission(0x77);
-  MD = Wire.read();
-  MD = MD << 8;
-  MD = MD | Wire.read();
-  Wire.endTransmission();
 
 
 
@@ -170,20 +92,28 @@ void loop() {
   Wire.write(0xF4);//SEND REGISTER ADDRESS
   Wire.write(0x2E);//write 2E into the address
 
-  vTaskDelay(5);//WAIT 5ms as per documentation
+  Wire.endTransmission(); //end transmission as we now want to read // this sends the writes, then we wait
+
+  delay(5);//WAIT 5ms as per documentation //replaced with just delay instead of vtaskdelay for debugging 
 
   short MSB = 0;
   short LSB = 0;
 
-  Wire.endTransmission(); //end transmission as we now want to read
+
 
   Wire.beginTransmission(0x77);
 
-  Wire.requestFrom(0xF6,1);
+  Wire.write(0xF6); // we want to read from f6 for MSB and F7 for lsb
+
+  Wire.endTransmission(false);
+
+  Wire.requestFrom(0x77,1);
   MSB = Wire.read();
 
-  Wire.requestFrom(0xF7,1);
+  Wire.requestFrom(0x77,1);
   LSB = Wire.read();
+
+  Serial.print("here");
 
   UT = MSB << 8 + LSB; //unsure about correct presidence here, shift should have higher presidence but that may not be intended
 
@@ -191,15 +121,24 @@ void loop() {
   //calculate true temperature
 
   long X1 = (UT - AC6) * AC5/(pow(2,15));
+
   long X2 = MC * (pow(2,11))/(X1+MD);
+
   long B5 = X1 + X2;
+
   T = (B5 + 8)/pow(2,4);// this should be the final temperature IN 0.1 DEG CEL
+
   T = T/10; //divide by 10 so we are in deg C
 
 
-  Serial.print("Temperature is: " + T);
+
+
+  Serial.print("Temperature is: ");
+  Serial.print(T);
   Serial.print(" degrees Celcius");
   Serial.println();//make a new line after enverything
+
+  delay(100); //delay by 100ms so we are not being overwhelmed
 
 
 
